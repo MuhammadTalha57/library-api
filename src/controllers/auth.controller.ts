c9c2b0f6e75235323d5e100d6d74af8983dc9424
Muhammad Talha <muhammadtalha.mailme@gmail.com>
2026-03-10 00:06:29 +0500

    Implemented authentication

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,32 @@
+import { authenticateUserService, registerUserService } from "../services/auth.service"
+import { Request, Response } from "express"
+
+export const registerUser = async (req: Request, res: Response) => {
+    const user = await registerUserService(req.body)
+
+    if(user) {
+        res.status(201).json({
+            success: true,
+            data: user
+        })
+    } else {
+        res.status(400).json({
+            success: false
+        })
+    }
+}
+
+export const authenticateUser = async (req: Request, res: Response) => {
+    const user = await authenticateUserService(req.body)
+
+    if(user) {
+        res.status(200).json({
+            success: true,
+            data: user
+        })
+    } else {
+        res.status(401).json({
+            success: false
+        })
+    }
+}
---
+++ src/services/auth.service.ts
@@ -0,0 +1,35 @@
+import bcrypt from "bcrypt"
+import { generateToken } from "../utils/jwt"
+import { Role } from "../generated/prisma/enums"
+import prisma from "../config/prisma"
+
+
+export const registerUserService = async(data:any) => {
+
+    const hashedPassword = await bcrypt.hash(data.password, 10)
+
+    const user = await prisma.user.create({
+        data: {
+            name: data.name,
+            email: data.email,
+            password: hashedPassword,
+            role: Role.MEMBER,
+        }
+    })
+
+
+    return user;
+}
+
+
+export const authenticateUserService = async(data:any) => {
+    const user = await prisma.user.findFirst({where: {email: data.email}})
+
+    if(!user || !(await bcrypt.compare(data.password, user.password))) {
+        return null;
+    }
+
+    generateToken(user)
+    return user;
+    
+}
