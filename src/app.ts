aa23757eec4b2affd3556841a512a1597d3a62d9
Muhammad Talha <muhammadtalha.mailme@gmail.com>
2026-03-10 22:43:58 +0500

    Applied Rate Limiting

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,20 @@ import cors from "cors";
 import authRoutes from "./routes/auth.routes";
 import bookRoutes from "./routes/book.routes";
 import borrowRoutes from "./routes/borrow.routes";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 Minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: "Too many requests, please try again later.",
+    standardHeaders: true,
+});
+app.use(limiter);
 
 app.use("/auth", authRoutes);
 
